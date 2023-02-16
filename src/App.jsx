c099cf195feca8db67ad9265da6da78fa1147ec8
Pallab Majumdar <kitkatpallab@gmail.com>
2023-02-16 14:14:02 +0600

    R 1tu baki

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,45 @@
 import { useEffect, useState } from 'react';
 import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
 import axios from 'axios';
 function App() {
   const [images, setImage] = useState([]);
-  const url =
-    'https://pixabay.com/api/?key=21437647-4652ee9a0fdba064aa4b23c20&q=random&image_type=photo&pretty=true';
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [term, setTerm] = useState('');
   useEffect(() => {
     setTimeout(() => {
       axios({
-        url: url,
+        url: `https://pixabay.com/api/?key=21437647-4652ee9a0fdba064aa4b23c20&q=${term}&image_type=photo&pretty=true`,
       })
         .then((res) => {
-          console.log(res.data.hits);
+          //console.log(res);
           setImage(res.data.hits);
+          setIsLoading(false);
         })
         .catch((err) => {
-          setIsPending(false);
           setError(err.message);
         });
     }, 1000);
-  }, []);
+  }, [term]);
 
   return (
     <div className="App mx-auto">
-      <div className="flex flex-row flex-wrap justify-center">
-        {images.map((image) => (
-          <ImageCard key={image.id} image={image}></ImageCard>
-        ))}
+      <ImageSearch searchText={(text) => setTerm(text)} />
+      {!isLoading && images.length === 0 && (
+        <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>
+      )}
+
+      <div className="flex justify-center">
+        {isLoading ? (
+          <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
+        ) : (
+          <div className="columns-1 md:columns-2 lg:columns-4 justify-center gap-1">
+            {images.map((image) => (
+              <ImageCard key={image.id} image={image}></ImageCard>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,33 +1,41 @@
-const ImageCard = ({image}) => {
-    const tags=image.tags.split(',')
-    return (  
-        <div className="max-w-sm rounded-lg overflow-hidden shadow-lg">
-            <img src={image.webformatURL} alt="" className="w-full" />
-            <div className="px-4 py-4">
-                <div className="font-bold text-purple-500 text-xl mb-2">
-                    Photo By {image.user}
-                </div>
-                <ul>
-                    <li>
-                        <strong>Views:</strong>{image.views}
-                    </li>
-                    <li>
-                        <strong>Downloads:</strong>{image.downloads}
-                    </li>
-                    <li>
-                        <strong>Likes:</strong>{image.likes}
-                    </li>
-                </ul>
-            </div>
-            <div className="tags flex mb-4">
-                {
-                    tags.map((tag,index) =>(
-                        <span key={index} className="inline-block bg-gray-200 rounded-full mx-2 p-1">#{tag}</span>
-                    ))
-                }
-            </div>
+const ImageCard = ({ image }) => {
+  const tags = image.tags.split(',');
+  return (
+    <div className="max-w-sm rounded-lg overflow-hidden shadow-lg mr-2 ml-2 mb-2 flex flex-col">
+      <div className="photo">
+      <img src={image.webformatURL} alt="" className="w-full h-full object-cover" />
+      </div>
+      <div className="px-4 py-4 flex flex-col">
+        <div className="font-bold text-purple-500 text-xl mb-2 flex flex-row">
+          Photo By {image.user}
         </div>
-    );
-}
- 
+        <ul>
+          <li>
+            <strong>Views:</strong>
+            {image.views}
+          </li>
+          <li>
+            <strong>Downloads:</strong>
+            {image.downloads}
+          </li>
+          <li>
+            <strong>Likes:</strong>
+            {image.likes}
+          </li>
+        </ul>
+      </div>
+      <div className="tags flex mb-4 flex-wrap">
+        {tags.map((tag, index) => (
+          <span
+            key={index}
+            className=" bg-gray-200 rounded-2xl mx-1 px-2 py-1 text-center my-2 text-ellipsis text-sm"
+          >
+            #{tag}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
 export default ImageCard;
